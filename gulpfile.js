edbf173fccd14aff924f661c30685b82f0b34806
Ya'ar Hever <yaarhever@gmail.com>
2016-05-08 14:44:31 +0200

    - browserify - split between desktop and mobile - gulp story: create one minified html of everything

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,66 @@
+'use strict';
 var gulp = require('gulp');
 
+var browserify = require('browserify');
+
 var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 var modernizr = require('gulp-modernizr');
 
-gulp.task('lint', function () {
-    return gulp.src('src/js/*.js')
-            .pipe(jshint())
-            .pipe(jshint.reporter('default'));
+var source = require('vinyl-source-stream');
+
+var inlineCSS = require('gulp-inline-css');
+
+var inlinesource = require('gulp-inline-source');
+
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('lint', function() {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('modernizr', function () {
-    return gulp.src('src/js/*.js')
-            .pipe(modernizr())
-            .pipe(gulp.dest('build/'))
-            .pipe(uglify())
-            .pipe(gulp.dest('build/js'));
+gulp.task('inline-css', function() {
+    return gulp.src('src/*.html')
+        .pipe(inlineCSS({
+            removeStyleTags: false
+        }))
+        .pipe(gulp.dest('out'));
 });
 
-gulp.task('scripts', function () {
-    return gulp.src('src/js/*.js')
-            .pipe(concat('all.js'))
-            .pipe(gulp.dest('build'))
-            .pipe(rename('all.min.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('build/js'));
+gulp.task('modernizr', function() {
+    return gulp.src('src/*.js')
+        .pipe(modernizr({
+            tests: ['videoautoplay']
+        }))
+        .pipe(gulp.dest('out'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+gulp.task('browserify', function() {
+    return browserify('src/catandpaste.js')
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('out'));
 });
 
-gulp.task('default', ['lint', 'scripts', 'watch']);
+gulp.task('bundle', ['inline-css', 'modernizr', 'browserify'], function() {
+    return gulp.src('out/index.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('dist'))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            collapseInlineTagWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/*', ['lint', 'bundle']);
+});
+
+gulp.task('default', ['lint', 'bundle', 'watch']);
